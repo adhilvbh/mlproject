b427adc88da926aca6b81e9317fb6fff860a6eed
adhil aseem <adhilaseem@adhils-MacBook-Air.local>
2024-03-25 14:10:55 +0530

    flask ui

--- application.py
+++ application.py
@@ -1,47 +1,49 @@
-import gradio as gr
+from flask import Flask, request, render_template
+import numpy as np
 import pandas as pd
 
+from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-# Define function to make predictions
+application = Flask(__name__)
+
+app = application
+
+# Route for a home page
 
 
-def predict_datapoint(gender, race_ethnicity, parental_level_of_education, lunch, test_preparation_course, reading_score, writing_score):
-    data = CustomData(
-        gender=gender,
-        race_ethnicity=race_ethnicity,
-        parental_level_of_education=parental_level_of_education,
-        lunch=lunch,
-        test_preparation_course=test_preparation_course,
-        reading_score=float(reading_score),
-        writing_score=float(writing_score)
-    )
-    pred_df = data.get_data_as_data_frame()
-    predict_pipeline = PredictPipeline()
-    results = predict_pipeline.predict(pred_df)
-    return results[0]
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-# Define inputs
-gender = gr.Dropdown(choices=["female", "male"], label="Gender")
-race_ethnicity = gr.Dropdown(choices=[
-    "group A", "group B", "group C", "group D", "group E"], label="Race/Ethnicity")
-parental_level_of_education = gr.Dropdown(choices=["some high school", "high school", "some college",
-                                                   "associate's degree", "bachelor's degree", "master's degree"], label="Parental Level of Education")
-lunch = gr.Dropdown(choices=["standard", "free/reduced"], label="Lunch")
-test_preparation_course = gr.Dropdown(
-    choices=["none", "completed"], label="Test Preparation Course")
-reading_score = gr.Slider(
-    minimum=0, maximum=100, label="Reading Score")
-writing_score = gr.Slider(
-    minimum=0, maximum=100, label="Writing Score")
+@app.route('/predictdata', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get(
+                'parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get(
+                'test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
 
-# Create interface
-iface = gr.Interface(fn=predict_datapoint,
-                     inputs=[gender, race_ethnicity, parental_level_of_education,
-                             lunch, test_preparation_course, reading_score, writing_score],
-                     outputs="text",
-                     title="Student Performance Predictor")
+        )
+        pred_df = data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
 
-# Launch the interface
-iface.launch(share=True)
+        predict_pipeline = PredictPipeline()
+        print("Mid Prediction")
+        results = predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html', results=results[0])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
